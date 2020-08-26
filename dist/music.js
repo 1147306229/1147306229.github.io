63c80e5ac926a4bd756bf61553d34f7f55448fa1
山海 <1147306229@qq.com>
2020-08-26 20:33:17 +0800

    Site updated: 2020-08-26 20:33:13

---
+++ dist/music.js
@@ -0,0 +1,15 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    lrcType: 3,
+    audio: [
+      {
+        name: "千禧",
+        artist: '徐秉龙',
+        url: '//music.163.com/outchain/player?type=2&id=522510615&auto=1&height=66',
+        cover: 'http://p1.music.126.net/_sblL2ZjBOtwLAL-3i01Fw==/109951163078474921.jpg?param=130y130',
+     
+      },
+    ]
+});
