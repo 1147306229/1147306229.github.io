fb8652b05494348c364db16f189bb1a37483d565
山海 <1147306229@qq.com>
2020-08-28 08:54:12 +0800

    Site updated: 2020-08-28 08:54:10

--- dist/music.js
+++ dist/music.js
@@ -1,15 +1,69 @@
-const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    fixed: true,
-    autoplay: false,
-    lrcType: 3,
-    audio: [
-      {
-        name: "千禧",
-        artist: '徐秉龙',
-        url: 'https://music.163.com/outchain/player?type=2&id=519136840&auto=1&height=66',
-        cover: 'http://p1.music.126.net/_sblL2ZjBOtwLAL-3i01Fw==/109951163078474921.jpg?param=130y130',
-     
-      },
-    ]
-});
+var aplayers = [],
+	loadMeting = function() {
+		function a(a, b) {
+			var c = {
+				container: a,
+				audio: b,
+				mini: null,
+				fixed: null,
+				autoplay: !1, //控制自动播放
+				mutex: !0,
+				lrcType: 3,
+				listFolded: !1,
+				preload: 'auto',
+				theme: '#2980b9',
+				loop: 'all',
+				order: 'list', 
+				volume: null,
+				listMaxHeight: null,
+				customAudioType: null,
+				storageName: 'metingjs'
+			};
+			if (b.length) {
+				b[0].lrc || (c.lrcType = 0);
+				var d = {};
+				for (var e in c) {
+					var f = e.toLowerCase();
+					(a.dataset.hasOwnProperty(f) || a.dataset.hasOwnProperty(e) || null !== c[e]) && (d[e] = a.dataset[f] || a.dataset[
+						e] || c[e], ('true' === d[e] || 'false' === d[e]) && (d[e] = 'true' == d[e]))
+				}
+				aplayers.push(new APlayer(d))
+			}
+		}
+		var b = 'https://api.i-meto.com/meting/api?server=:server&type=:type&id=:id&r=:r';
+		'undefined' != typeof meting_api && (b = meting_api);
+		for (var f = 0; f < aplayers.length; f++) try {
+			aplayers[f].destroy()
+		} catch (a) {
+			console.log(a)
+		}
+		aplayers = [];
+		for (var c = document.querySelectorAll('.aplayer'), d = function() {
+				var d = c[e],
+					f = d.dataset.id;
+				if (f) {
+					var g = d.dataset.api || b;
+					g = g.replace(':server', d.dataset.server), g = g.replace(':type', d.dataset.type), g = g.replace(':id', d.dataset
+						.id), g = g.replace(':auth', d.dataset.auth), g = g.replace(':r', Math.random());
+					var h = new XMLHttpRequest;
+					h.onreadystatechange = function() {
+						if (4 === h.readyState && (200 <= h.status && 300 > h.status || 304 === h.status)) {
+							var b = JSON.parse(h.responseText);
+							a(d, b)
+						}
+					}, h.open('get', g, !0), h.send(null)
+				} else if (d.dataset.url) {
+					var i = [{
+						name: d.dataset.name || d.dataset.title || 'Audio name',
+						artist: d.dataset.artist || d.dataset.author || 'Audio artist',
+						url: d.dataset.url,
+						cover: d.dataset.cover || d.dataset.pic,
+						lrc: d.dataset.lrc,
+						type: d.dataset.type || 'auto'
+					}];
+					a(d, i)
+				}
+			}, e = 0; e < c.length; e++) d()
+	};
+document.addEventListener('DOMContentLoaded', loadMeting, !1);
+document.addEventListener('DOMContentLoaded', loadMeting, !1);
